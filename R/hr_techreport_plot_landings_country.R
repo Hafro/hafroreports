c4d3e0f3cf8767d5d523fa71532d893bd0518943
Jamie Lentin <jm@lentin.co.uk>
2026-03-25 20:37:58 +0000

    hr_techreport_plot_landings_country: Add from 02-had

---
+++ R/hr_techreport_plot_landings_country.R
@@ -0,0 +1,22 @@
+hr_techreport_plot_landings_country <- function(
+  pcon,
+  ylab = 'Landings (in kt)',
+  xlab = 'Year',
+  breaks = seq(0, 1e5, by = 10)
+) {
+  dplyr::tbl(pcon, "landings") |>
+    dplyr::mutate(country = ifelse(country != 'Iceland', 'Other', 'Iceland')) |>
+    dplyr::group_by(year, country) |>
+    dplyr::summarize(catch = sum(catch, na.rm = TRUE) / 1e3) |>
+    dplyr::arrange(dplyr::desc(country)) |>
+    ggplot2::ggplot(ggplot2::aes(year, catch, fill = country)) +
+    ggplot2::geom_bar(stat = 'identity') +
+    ggplot2::theme_bw() +
+    ggplot2::labs(y = ylab, x = xlab, fill = '') +
+    ggplot2::theme(
+      legend.background = ggplot2::element_blank(),
+      legend.position = c(0.15, 0.75)
+    ) +
+    ggplot2::scale_x_continuous(breaks = breaks) +
+    pax::pax_scale_fill_crayola()
+}
