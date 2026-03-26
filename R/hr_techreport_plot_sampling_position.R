5f8b111b4a10295a8b2e49bd79b5867e9ce6a2f9
Jamie Lentin <jm@lentin.co.uk>
2026-03-26 13:51:52 +0000

    hr_techreport_plot_sampling_position: Start/end year filter

--- R/hr_techreport_plot_sampling_position.R
+++ R/hr_techreport_plot_sampling_position.R
@@ -1,19 +1,27 @@
 hr_techreport_plot_sampling_position <- function(
   pcon,
   mfdb_gear_codes = c('LLN', 'DSE', 'BMT'),
-  assessment_year
+  year_start = 2000,
+  year_end = 2000
 ) {
   sampling_pos <- dplyr::tbl(pcon, "sampling") |>
+    dplyr::filter(
+      year >= local(year_start),
+      year <= local(year_end),
+      mfdb_gear_code %in% mfdb_gear_codes
+    ) |>
     dplyr::left_join(dplyr::tbl(pcon, "measurement"), by = "sample_id") |>
     dplyr::mutate(lon = round(lon, 1), lat = round(lat, 1)) |>
     dplyr::select(lat, lon, year, mfdb_gear_code) |>
     dplyr::distinct() |>
+    pax::pax_describe_mfdb_gear_code() |>
     dplyr::collect()
 
   catch_by_gear <-
     dplyr::tbl(pcon, "logbook") |>
     dplyr::filter(
-      year == local(assessment_year - 1),
+      year >= local(year_start),
+      year <= local(year_end),
       mfdb_gear_code %in% mfdb_gear_codes
     ) |>
     dplyr::mutate(lon = round(lon, 1), lat = round(lat, 1)) |>
@@ -28,7 +36,7 @@ hr_techreport_plot_sampling_position <- function(
     pax::pax_map_layer_depth(dplyr::tbl(pcon, "ocean_depth")) |>
     pax::pax_map_layer_catch(
       catch_by_gear,
-      annotation = 'year', # TODO: gear?
+      annotation = 'gear',
       na.fill = -50
     ) +
     ggplot2::geom_point(ggplot2::aes(lon, lat), data = sampling_pos, pch = 4)
