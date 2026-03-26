b87e9e61ce6f087edaec148924e45b4ec533c515
Jamie Lentin <jm@lentin.co.uk>
2026-03-26 13:52:26 +0000

    hr_techreport_plot_catch_region: Region plot

---
+++ R/hr_techreport_plot_catch_region.R
@@ -0,0 +1,37 @@
+# Was catch_by_area_plot
+hr_techreport_plot_catch_region <- function(
+  pcon,
+  year_start = 1000,
+  year_end = 9999
+) {
+  dplyr::tbl(pcon, "logbook") |>
+    dplyr::filter(year >= year_start, year <= year_end) |>
+    pax::pax_add_ocean_depth_class(breaks = c(0, 100, 200, 300)) |>
+    pax::pax_add_regions(
+      regions = list(
+        W = 101,
+        NW = 102,
+        NE = c(103, 104, 105),
+        SE = c(107, 106),
+        SW = 108
+      )
+    ) |>
+    dplyr::mutate(region = coalesce(region, 'Other')) |>
+    dplyr::group_by(year, mfdb_gear_code, region, ocean_depth_class) |>
+    dplyr::summarise(val = sum(catch, na.rm = TRUE) / 1e6) |>
+    dplyr::rename(group = region) |>
+    dplyr::ungroup() |>
+    dplyr::collect() |>
+    two_panel_plot(
+      cols = c(
+        "#999999",
+        "#E69F00",
+        "#56B4E9",
+        "#009E73",
+        "#F0E442",
+        "#0072B2",
+        "#D55E00",
+        "#CC79A7"
+      )
+    )
+}
