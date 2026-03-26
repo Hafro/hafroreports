d46a2a05ce8b674bb09eb760c8f52084e976db09
Jamie Lentin <jm@lentin.co.uk>
2026-03-26 13:52:05 +0000

    hr_techreport_table_boatsummary: Start/end year filter

--- R/hr_techreport_table_boatsummary.R
+++ R/hr_techreport_table_boatsummary.R
@@ -1,8 +1,10 @@
 hr_techreport_table_boatsummary <- function(
   pcon,
-  year_start = 1000
+  year_start = 1000,
+  year_end = 9999
 ) {
   dplyr::tbl(pcon, "landings") |>
+    dplyr::filter(year >= year_start, year <= year_end) |>
     pax::pax_landings_by_gear() |>
     dplyr::ungroup() |>
     dplyr::filter(
