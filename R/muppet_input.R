1bddd845af078d614c39e6cf4dd65e551f2ae3b1
Jamie Lentin <jm@lentin.co.uk>
2026-03-31 11:19:18 +0000

    muppet_input: rmuppet::line_replace now public

--- R/muppet_input.R
+++ R/muppet_input.R
@@ -12,17 +12,17 @@ hr_muppet_input_optionfile <- function(
     strsplit("\n") |>
     unlist() |>
     stringr::str_remove('../') |>
-    rmuppet:::line_replace(age_end, '# Last model age') |>
-    rmuppet:::line_replace(plus_group, '# Plus group') |>
-    rmuppet:::line_replace(
+    rmuppet::line_replace(age_end, '# Last model age') |>
+    rmuppet::line_replace(plus_group, '# Plus group') |>
+    rmuppet::line_replace(
       year_end - 1,
       '# Last data year, last year with catch at age data'
     ) |>
-    rmuppet:::line_replace(
+    rmuppet::line_replace(
       year_end - 1,
       '# Last opt year i.e last year before assyear   <=lastdatayear'
     ) |>
-    rmuppet:::line_replace(year_end - 1, '# Last year smh') |>
-    rmuppet:::line_replace(year_end, '# Last year smb')
+    rmuppet::line_replace(year_end - 1, '# Last year smh') |>
+    rmuppet::line_replace(year_end, '# Last year smb')
   return(out_files)
 }
